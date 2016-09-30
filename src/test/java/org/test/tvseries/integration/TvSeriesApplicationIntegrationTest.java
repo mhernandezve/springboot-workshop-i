30c17d94fc6b5821dba6d38ee4f5964e40100751
faustocv <fausto.castaneda.v@gmail.com>
2016-09-30 18:15:13 -0500

    Adds integration tests

---
+++ src/test/java/org/test/tvseries/integration/TvSeriesApplicationIntegrationTest.java
@@ -0,0 +1,25 @@
+package org.test.tvseries.integration;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+public class TvSeriesApplicationIntegrationTest {
+
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+  @Test
+  public void shouldReturnAnEmptyListGivenGetAllTvSeriesRequest(){
+    String tvSeries = restTemplate.getForObject("/test/tvseries", String.class);
+    Assert.assertThat(tvSeries , Matchers.is("[]"));
+  }
+}
