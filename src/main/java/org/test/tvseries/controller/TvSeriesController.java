af2807a9d869834676f398b81578bdf12ab1f904
faustocv <fausto.castaneda.v@gmail.com>
2016-09-30 16:12:08 -0500

    Refactors unit tests of tv series controller

--- src/main/java/org/test/tvseries/controller/TvSeriesController.java
+++ src/main/java/org/test/tvseries/controller/TvSeriesController.java
@@ -1,19 +1,27 @@
 package org.test.tvseries.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.test.tvseries.entity.TvSerie;
 import org.test.tvseries.repository.TvSeriesRepository;
 
+@RestController
+@RequestMapping("/test/tvseries")
 public class TvSeriesController {
 
   @Autowired
   private TvSeriesRepository repository;
 
+  @RequestMapping(value="", method=RequestMethod.GET)
   public Iterable<TvSerie> getAllTvSeries() {
     return repository.findAll();
   }
 
-  public TvSerie getByName(String name) {
+  @RequestMapping(value="/name/{name}", method=RequestMethod.GET)
+  public TvSerie getByName(@PathVariable String name) {
     return repository.findByName(name);
   }
 }
--- src/test/java/org/test/tvseries/controller/TvSeriesControllerTest.java
+++ src/test/java/org/test/tvseries/controller/TvSeriesControllerTest.java
@@ -1,41 +1,49 @@
 package org.test.tvseries.controller;
 
-import java.util.ArrayList;
+import static java.util.Arrays.asList;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 import org.test.tvseries.entity.TvSerie;
 import org.test.tvseries.repository.TvSeriesRepository;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
-
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
+@WebMvcTest(TvSeriesController.class)
 public class TvSeriesControllerTest {
 
-  @Mock
-  private TvSeriesRepository tvSeriesRepository;
-
   @Autowired
-  @InjectMocks
-  private TvSeriesController tvSeriesController;
+  private MockMvc mockMvc;
+
+  @MockBean
+  private TvSeriesRepository repository;
 
   @Test
-  public void shouldReturnAllTvSeries(){
-    Iterable<TvSerie> tvSeries = new ArrayList<>();
-    when(tvSeriesRepository.findAll()).thenReturn(tvSeries);
-    assertEquals(tvSeries, tvSeriesController.getAllTvSeries());
+  public void shouldReturnTvSeriesGivenGetAllTvSeriesRequest() throws Exception {
+    String name = "someSerie";
+    Iterable<TvSerie> tvSeries = asList(new TvSerie(name));
+    given(repository.findAll()).willReturn(tvSeries);
+    mockMvc.perform(get("/test/tvseries").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+      .andExpect(jsonPath("$..name").value(name));
   }
 
   @Test
-  public void shouldReturnTvSerieByName() {
-    TvSerie tvSerie = new TvSerie();
-    String name = "someName";
-    when(tvSeriesRepository.findByName(name)).thenReturn(tvSerie);
-    assertEquals(tvSerie, tvSeriesController.getByName(name));
+  public void shouldReturnAtvSerieGivenGetByNameRequest() throws Exception {
+    String name = "someSerie";
+    TvSerie tvSerie = new TvSerie(name);
+    given(repository.findByName(name)).willReturn(tvSerie);
+    mockMvc.perform(get("/test/tvseries/name/" + name).accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$..name").value(name));
   }
 }
